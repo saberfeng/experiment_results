4c475913f6c931acabb00f847750fe495aab5c41
xingbo <saberfeng@outlook.com>
2020-10-13 17:28:36 +1000

    initial

---
+++ copy_nmap_results.sh
@@ -0,0 +1,11 @@
+docker exec mn.r1 sh -c "mkdir nmap_outputs"
+
+while true
+do 
+    docker exec mn.r1 sh -c "mv *.xml nmap_outputs"
+    docker cp mn.r1:/root/nmap_outputs/. ./FRVM
+    now=$(date +"%T")
+    echo "Copied : $now"
+    sleep 2m
+done 
+# # docker cp mn.r1:/root/nmap_outputs/. ./nmap_outputs/simple_switch_13
