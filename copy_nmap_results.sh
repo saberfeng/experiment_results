b46a8d97a4d156d918ca3a98fb20a8160e3e7f90
xingbo <saberfeng@outlook.com>
2020-10-16 02:52:29 +1000

    sS-300-false, sT-300-false, sU-300-false, sS-300-true, index 392-779

--- copy_nmap_results.sh
+++ copy_nmap_results.sh
@@ -3,7 +3,7 @@ docker exec mn.r1 sh -c "mkdir nmap_outputs"
 while true
 do 
     docker exec mn.r1 sh -c "mv *.xml nmap_outputs"
-    docker cp mn.r1:/root/nmap_outputs/. ./FRVM
+    docker cp mn.r1:/root/nmap_outputs/. ./FRVM_200s
     now=$(date +"%T")
     echo "Copied : $now"
     sleep 2m
--- count_open.sh
+++ count_open.sh
@@ -4,7 +4,7 @@ do
     grep -o 'open' FRVM/nmap_Time300_ScansS_Portdiscovered_$i.xml | wc -l
 done
 
-# for i in {1..15}
-# do
-#     grep -o 'open' nmap_Time300_ScansS_Portdiscovered_$i.xml | wc -l
-# done
+for i in {778..797}
+do
+    grep -o 'open' nmap_Time300_ScansS_Portdiscovered_$i.xml | wc -l
+done
