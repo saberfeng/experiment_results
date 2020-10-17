ea82bad1374611317d8479212a5494e38ae2e41c
xingbo <saberfeng@outlook.com>
2020-10-17 15:20:06 +1000

    sS-100-false, sT-100-false, sU-100-false, sS-100-true, index 50-425; sS-400-false, sT-400-false, sU-400-false, sS-400-true, index 50-347

--- copy_nmap_results.sh
+++ copy_nmap_results.sh
@@ -3,7 +3,7 @@ docker exec mn.r1 sh -c "mkdir nmap_outputs"
 while true
 do 
     docker exec mn.r1 sh -c "mv *.xml nmap_outputs"
-    docker cp mn.r1:/root/nmap_outputs/. ./FRVM_200s
+    docker cp mn.r1:/root/nmap_outputs/. ./FRVM_400s
     now=$(date +"%T")
     echo "Copied : $now"
     sleep 2m
